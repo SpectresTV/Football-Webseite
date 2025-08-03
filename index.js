873337125ea250e6964e302334eb8e32b4072db9
SpectresTV <martinmux811@gmail.com>
2025-08-03 13:56:43 +0200

    chore: ensure static assets resolve from project root

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ db.serialize(() => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'secret-key',
   resave: false,
